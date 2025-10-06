06d8f8f9ae6a9437d9b7c12cac35d04d9ea86246
MobeenButt <mobeen914butt@gmail.com>
2025-10-06 11:01:48 +0500

    Db connection with mongodb atlas

--- Project/src/db/index.js
+++ Project/src/db/index.js
@@ -13,3 +13,17 @@
 // }
 
 // export default connectDB
+
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js";
+const connectDB=async ()=>
+{
+    try {
+    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    console.log(`Connection Established at Host ${connectionInstance.connection.host}`)
+    } catch (error) {
+        console.log("Error: ",error);
+    }
+}
+
+export default connectDB
--- Project/src/index.js
+++ Project/src/index.js
@@ -1,33 +1,30 @@
-// //import dotenv from "dotenv"
-// import dotenv from 'dotenv'
-// import connectDB from "./db/index.js"
-// dotenv.config()
-// connectDB()
-import mongoose from "mongoose";
-import dotenv from "dotenv"
-import { DB_NAME } from "./constants.js";
-// import connectDB from "./db/index.js"
+// import mongoose from "mongoose";
+// import dotenv from "dotenv"
+// import { DB_NAME } from "./constants.js";
+
 // dotenv.config();
-// connectDB()
-dotenv.config();
-(async ()=>{
-    try {
-          const mongoURI=process.env.MONGODB_URI;
-          if(!mongoURI)
-          {
-            throw new Error("MongoDB URI env is not correct");
-          }
-      const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-       console.log(`MongoDB connected successfully at Host ${connectionInstance.connection.host}`)
-    //    app.listen(process.env.PORT,()=>
-    // {
-    //     console.log(`App is listening to PORT ${process.env.PORT}`);
+// (async ()=>{
+//     try {
+//           const mongoURI=process.env.MONGODB_URI;
+//           if(!mongoURI)
+//           {
+//             throw new Error("MongoDB URI env is not correct");
+//           }
+//       const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//        console.log(`MongoDB connected successfully at Host ${connectionInstance.connection.host}`)
+//     //    app.listen(process.env.PORT,()=>
+//     // {
+//     //     console.log(`App is listening to PORT ${process.env.PORT}`);
 
         
-    // })
-    } catch (error) {
-        console.log("Error: ",error)
-        throw error;
-    }
-})()
+//     // })
+//     } catch (error) {
+//         console.log("Error: ",error)
+//         throw error;
+//     }
+// })()
     
+import connectDB from "./db/index.js"
+import dotenv from "dotenv"
+dotenv.config();
+connectDB()
