84373d699c373ef7fb90d2aaf510a60081c13a81
MobeenButt <mobeen914butt@gmail.com>
2025-10-17 02:27:49 +0500

    add user and video model

--- Project/src/db/index.js
+++ Project/src/db/index.js
@@ -7,23 +7,26 @@
 //           console.log( `MongoDB connection established at Host ${connectionInstance.connection.host}`)
 //     } catch (error) {
 //         console.log(`Error: ${error}`);
-        
-        
+
 //     }
 // }
 
 // export default connectDB
 
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-const connectDB=async ()=>
-{
-    try {
-    const connectionInstance= await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-    console.log(`Connection Established at Host ${connectionInstance.connection.host}`)
-    } catch (error) {
-        console.log("Error: ",error);
-    }
-}
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    console.log(
+      `Connection Established at Host ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("Error: ", error);
+    process.exit(1);
+  }
+};
 
-export default connectDB
+export default connectDB;
--- Project/src/index.js
+++ Project/src/index.js
@@ -16,15 +16,32 @@
 //     // {
 //     //     console.log(`App is listening to PORT ${process.env.PORT}`);
 
-        
 //     // })
 //     } catch (error) {
 //         console.log("Error: ",error)
 //         throw error;
 //     }
 // })()
-    
-import connectDB from "./db/index.js"
-import dotenv from "dotenv"
-dotenv.config();
+
+import connectDB from "./db/index.js";
+import dotenv from "dotenv";
+import { app } from "./app.js";
+dotenv.config({
+  path: "./.env",
+});
 connectDB()
+  .then(() => {
+    app.on("error",(error)=>
+    {
+        console.log("Error: ",error)
+        throw error
+    })
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo Db connection failed !!", err);
+  });
+
+// async code also returns a promise therefore in connectDB it returns a promise also
